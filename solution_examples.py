1caa07ea2dd2343d996f394b61f447e4df6c5dbf
doctorwait <101414476+doctorwait@users.noreply.github.com>
2022-04-06 20:57:13 +1000

    Create solution_examples.py

---
+++ solution_examples.py
@@ -0,0 +1,26 @@
+# Задания к решению подбираются случайным образом, поэтому описанные решения раскрывают только часть моих знаний.
+
+
+def sum_array(arr): # Функция описывает способ суммации элементов целочисленного массива без учёта в результате крайних по величинам чисел.
+    if arr is None:
+        return 0
+    elif len(arr) < 3:
+        print(len(arr))
+        return 0
+    else:
+        arr.pop(arr.index(max(arr)))
+        arr.pop(arr.index(min(arr)))
+        return sum(arr)
+      
+      
+def dna_to_rna(dna): # Конвертер ДНК в РНК, на вход принимается вложенный список
+    if len(dna) == 0:
+        return 0
+    else:
+        for nucleic in dna:
+            ind = dna.index(nucleic)
+            if nucleic == "T":
+                if ind == 0:
+                    dna = 'U' + dna[1:]
+                dna = dna[0:ind] + 'U' + dna[ind+1:]
+        return dna
