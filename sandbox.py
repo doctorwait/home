7713d4b30b3091e082b9c893defd73ac1fda46d7
doctorwait <prosnites@yandex.ru>
2022-07-23 20:41:27 +1000

    Удалил лишнее и обновил комментарии. Придумал следующее действие.

--- sandbox.py
+++ sandbox.py
@@ -5,13 +5,18 @@ from timer import timer
 
 class MagicSquare:
     def __init__(self, size):
+        # Минимальная сторона квадрата - 3, иначе он не считается "магическим"
         self.size = 3 if size <= 2 else size
+        # Формируем матрицу для экспериментов и её пустую копию для обнуления
         self.matrix = [[0] * self.size for i in range(self.size)]
         self.matrix_defaulter = list()
         for i in self.matrix:
             self.matrix_defaulter.append(i[:])
+        # Максимальное число в ячейке равно квадрату стороны
         self.max_random = self.size * self.size
+        # Генерируем координаты точек, лежащих на диагонали
         self.diagonals = self.diagonals_coords()
+        # Словарь с информацией о каждом числе - находится ли оно в матрице в конкретный период времени. Можно обнулять.
         self.numbers_dict = dict((i, False) for i in range(1, self.max_random + 1))
         self.numbers_dict_defaulter = dict()
         for i in self.numbers_dict:
@@ -21,14 +26,10 @@ class MagicSquare:
     def sums_of_lines(matrix: list, size: int, results=None, first_lines_for_success=False):
         """
         Возвращает список из сумм: 1-й и 2-й элементы = суммы по диагоналям, далее N элементов суммы горизонталей, и N
-        элементов - по вертикали.
-        Дополнительно, используется для проверки в методе success, который предохраняет от бесконечного цикла заливку
-        периферии.
+        элементов - по вертикали, где N = стороне квадрата.
         """
         if results is None:
             results = [0]*(size * 2 + 2)
-        if first_lines_for_success:
-            return sum([matrix[0][i] for i in range(size)]), sum([matrix[j][0] for j in range(size)])
         for i in range(size):
             results[0] += matrix[i][i]
             results[1] += matrix[i][-i - 1]
@@ -46,7 +47,6 @@ class MagicSquare:
             self.matrix.append(i[:])
         for i in self.numbers_dict:
             self.numbers_dict[i] = self.numbers_dict_defaulter[i]
-
 
     def break_condition(self, switcher_for_diagonals=False):
         """
@@ -73,17 +73,6 @@ class MagicSquare:
                 return False
         return True
 
-    def success_condition(self):
-        """
-        Возвращает True, если есть хотя бы одна возможность составить нужную комбинацию на пересечении первых
-        горизонтали и вертикали (и эта комбинация равна сумме по диагонали). Необходимо применять после метода
-        fill_diagonals.
-        """
-        reference = sum([self.matrix[i][i] for i in range(self.size)])
-        horizontal, vertical = self.sums_of_lines(self.matrix, self.size, first_lines_for_success=True)
-        return True if reference == horizontal == vertical else False
-        # TODO Метод не готов; он должен высчитывать линии, а не просто сравнивать их. Но метод нигде пока не применён.
-
     def diagonals_coords(self):
         """
         Возвращает множество из кортежей с координатами всех точек на двух диагоналях квадрата. Формат (х, у).
@@ -101,7 +90,7 @@ class MagicSquare:
         """
         self.fill_diagonals()
         self.show()
-        #self.fill_periphery()
+        self.fill_periphery()
 
     @timer
     def fill_diagonals(self):
@@ -118,6 +107,7 @@ class MagicSquare:
             #  Если число для новой позиции уже стоит в другом месте квадрата
             if self.numbers_dict[d_number]:
                 continue
+            # Для старого числа нужно обнулить его "вакантность" - возможность переиспользования
             if self.matrix[x][y] != 0:
                 self.numbers_dict[self.matrix[x][y]] = False
             self.matrix[x][y] = d_number
@@ -128,16 +118,14 @@ class MagicSquare:
                     continue
                 elif self.break_condition(switcher_for_diagonals=True):
                     print('Считает диагонали равными:')
-                    switch = False
                     return
 
     def fill_periphery(self):
         """
-        Заливает точки, не принадлежащие линиям диагоналей. Предпринимает X**N попыток, где N - сторона квадрата, а X -
-        максимальный размер случайного числа. Если не удаётся провести заливку, выходит из цикла, что позволит
-         сгенерировать диагонали заново.
+        Заливает точки, не принадлежащие линиям диагоналей.
         """
         print('Начал заливать периферию')
+
         while not self.break_condition():
             a, b = randint(0, self.size - 1), randint(0, self.size - 1)
             if (a, b) in self.diagonals:
@@ -152,7 +140,8 @@ class MagicSquare:
             print(*self.matrix[i])
 
 
-res = MagicSquare(15)
+res = MagicSquare(5)
 res.fill_diagonals()
 res.show()
 
+# TODO Возможно, верный алгоритм - при создании диагоналей сразу же заполнять 4 крайние границы квадрата
